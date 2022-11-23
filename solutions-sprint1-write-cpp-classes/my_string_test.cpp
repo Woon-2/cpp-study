55f5901cb0529741cbc1c3988f86ad95b140c164
Lcat <ljj7403@naver.com>
2022-11-23 22:40:23 +0900

    test1

--- solutions-sprint1-write-cpp-classes/my_string_test.cpp
+++ solutions-sprint1-write-cpp-classes/my_string_test.cpp
@@ -1,15 +1,15 @@
 //#define _DEBUG
-#include "my_string_tests.h"
+#include "my_string_test_cases.h"
 // #include "alloc_log.h" // 메모리 누수 체크 필요시 주석 해제
 
 int main()
 {
-    //TEST1();
+    TEST1();
     //TEST2();
     //TEST3();
     //TEST4();
     //TEST5();
     //TEST6();
     //TEST7();
-    TEST8();
+    //TEST8();
 }
